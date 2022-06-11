5268d075700d1ca31d0ca7cee24f80f4f9198f41
apar <apargtm@gmail.com>
2022-06-11 19:25:05 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './Components/Header'
+
+function App(){
+    return (
+    <div>
+        <Header text="HOO"/>
+        <h1>APP HO YO MERO</h1>
+    </div>
+    )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+function Header(){
+    return (
+        <h1>FEEDBACK UI</h1>
+    )
+}
+
+// Header.defaultProps = {
+// }
+
+// Header.propTypes={
+//     text : PropTypes.string
+// }
+export default Header
